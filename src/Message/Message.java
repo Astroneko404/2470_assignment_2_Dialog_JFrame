66f7135e05da4860a69be819c411991c554749be
Astroneko404 <astroneko404@outlook.com>
2020-01-23 14:38:31 -0500

    param add

--- src/Message/Message.java
+++ src/Message/Message.java
@@ -8,49 +8,150 @@ public class Message extends JPanel{
     private static String COLOR_B;
     private static String COLOR_F;
     private static String FONT;
+    private static String FONT_SIZE;
+    private static String HEIGHT;
     private static String SHAPE;
     private static String TEXT;
+    private static String WIDTH;
+
+    Boolean color_b_err = false;
+    Boolean color_f_err = false;
+    Boolean font_err = false;
+    Boolean height_err = false;
+    Boolean shape_err = false;
+    Boolean text_err = false;
+    Boolean width_err = false;
+
+    String error_message = "Error:\n";
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException | NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
 
     public void paint(Graphics g){
-        if(COLOR_B.equals("blue")){
-            g.setColor(Color.blue);
-        }
-        else if(COLOR_B.equals("red")){
-            g.setColor(Color.red);
-        }
-        if (SHAPE.equals("oval")){
-            g.fillOval( 50, 40, 150, 80 );
+        switch (COLOR_B) {
+            case "black":
+                g.setColor(Color.black);
+                break;
+            case "blue":
+                g.setColor(Color.blue);
+                break;
+            case "cyan":
+                g.setColor(Color.cyan);
+                break;
+            case "dark gray":
+                g.setColor(Color.darkGray);
+                break;
+            case "gray":
+                g.setColor(Color.gray);
+                break;
+            case "green":
+                g.setColor(Color.green);
+                break;
+            case "light gray":
+                g.setColor(Color.lightGray);
+                break;
+            case "megenta":
+                g.setColor(Color.magenta);
+                break;
+            case "orange":
+                g.setColor(Color.orange);
+                break;
+            case "pink":
+                g.setColor(Color.pink);
+                break;
+            case "red":
+                g.setColor(Color.red);
+                break;
+            case "white":
+                g.setColor(Color.white);
+                break;
+            case "yellow":
+                g.setColor(Color.yellow);
+                break;
+            default:
+                color_b_err = true;
+                error_message = error_message + " - Background color value\n";
+                break;
         }
 
+        if (!isInteger(WIDTH)) {
+            width_err = true;
+        }
+        if (!isInteger(HEIGHT)) {
+            height_err = true;
+        }
 
+        if (!color_b_err && !width_err && !height_err) {
+            switch (SHAPE) {
+                case "oval":
+                    g.fillOval(
+                            (500 - Integer.parseInt(WIDTH)) / 2,
+                            (300 - Integer.parseInt(HEIGHT)) / 2,
+                            Integer.parseInt(WIDTH),
+                            Integer.parseInt(HEIGHT)
+                    );
+                    break;
+                case "rectangle":
+                    g.fillRect(
+                            (500 - Integer.parseInt(WIDTH)) / 2,
+                            (300 - Integer.parseInt(HEIGHT)) / 2,
+                            Integer.parseInt(WIDTH),
+                            Integer.parseInt(HEIGHT)
+                    );
+                    break;
+                case "rounded":
+                    break;
+                default:
+                    shape_err = true;
+                    error_message = error_message + " - Shape value\n";
+                    break;
+            }
+        }
     }
     public static void main(String[] args){
         JFrame f = new JFrame("Message");
         JPanel main = new JPanel();
         JPanel setting = new JPanel();
         JFrame f2 = new JFrame("Draw");
-        GridLayout grid = new GridLayout(7,2);
+        GridLayout grid = new GridLayout(10,2);
 
-        JLabel text = new JLabel("Text:");
-        JTextField text_field = new JTextField(20);
-        JLabel shape = new JLabel("Shape:");
-        JTextField shape_field = new JTextField(20);
-        JLabel color_back = new JLabel("Color - Background");
+        JLabel color_back = new JLabel("Color - Background:");
         JTextField color_back_field = new JTextField(20);
-        JLabel color_front = new JLabel("Color - Front");
+        JLabel color_front = new JLabel("Color - Front:");
         JTextField color_front_field = new JTextField(20);
         JLabel font = new JLabel("Font style:");
         JTextField font_field = new JTextField(20);
+        JLabel font_size = new JLabel("Font size:");
+        JTextField font_size_field = new JTextField(20);
+        JLabel height = new JLabel("Height:");
+        JTextField height_field = new JTextField(20);
+        JLabel shape = new JLabel("Shape:");
+        JTextField shape_field = new JTextField(20);
+        JLabel text = new JLabel("Text:");
+        JTextField text_field = new JTextField(20);
+        JLabel width = new JLabel("Width:");
+        JTextField width_field = new JTextField(20);
+
         JButton draw = new JButton("Draw");
+        draw.setSize(20,20);
 
         draw.addActionListener(actionEvent -> {
+            COLOR_B = color_back_field.getText().toLowerCase();
+            COLOR_F = color_front_field.getText().toLowerCase();
+            FONT = font_field.getText().toLowerCase();
+            FONT_SIZE = font_size_field.getText();
+            HEIGHT = height_field.getText();
+            SHAPE = shape_field.getText().toLowerCase();
             TEXT = text_field.getText();
-            SHAPE = shape_field.getText();
-            COLOR_B = color_back_field.getText();
-            COLOR_F = color_front_field.getText();
-            FONT = font_field.getText();
+            WIDTH = width_field.getText();
 
-            f2.setSize(300, 160);
+            f2.setSize(500, 300);
             f2.add(new Message());
             f2.setVisible(true);
         });
@@ -58,17 +159,27 @@ public class Message extends JPanel{
         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
 
         setting.setLayout(grid);
-        setting.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
-        setting.add(text);
-        setting.add(text_field);
-        setting.add(shape);
-        setting.add(shape_field);
+        setting.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
         setting.add(color_back);
         setting.add(color_back_field);
+        setting.add(shape);
+        setting.add(shape_field);
+        setting.add(width);
+        setting.add(width_field);
+        setting.add(height);
+        setting.add(height_field);
+
         setting.add(color_front);
         setting.add(color_front_field);
         setting.add(font);
         setting.add(font_field);
+        setting.add(font_size);
+        setting.add(font_size_field);
+
+        setting.add(text);
+        setting.add(text_field);
+
         setting.add(new JLabel(" "));
         setting.add(new JLabel(" "));
         setting.add(new JLabel(" "));
